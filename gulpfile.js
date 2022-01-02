080f808778ebb37ded6d0351567e6871f9754225
Melquiades <melquiadeshreyes@gmail.com>
2022-01-02 11:24:41 +0000

    sass structure of files and folders and gulpfile task, see changes in real time

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 /* ==================== EXAMPLE FOR TASKS
@@ -14,12 +14,20 @@ NOTE: WE CAN HAVE MANY TASKS
 
 //*** PROJECT TASKS IN GULPFILE.JS ***// 
 
-function css(done){
-    src('src/scss/app.scss') // Identify the .scss file that I am going to use
+function taskCSS(done){
+    src('src/scss/**/*.scss') // Identify the .scss file that I am going to use
         .pipe( sass() ) // Compile file
         .pipe( dest('build/css') ); // Save file to hard disk
 
     console.log("Task for compile file .scss to .css ...");
     done();
 }
-exports.css = css; 
+
+function taskWatch(done) {
+
+    watch('src/scss/**/*.scss', taskCSS);
+    console.log("Task for watch...");
+    done();
+}
+exports.taskCSS = taskCSS; 
+exports.taskWatch = taskWatch;
