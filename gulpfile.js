333e7ac008e55c069d9f34e2ff243c74bdd33963
Melquiades <melquiadeshreyes@gmail.com>
2022-01-16 14:10:02 +0000

    Created a section to put a DJ image

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require('gulp');
+// CSS
+const { src, dest, watch, parallel, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Images
+const cache = require('gulp-cache');
+
+/* npm install --save-dev gulp-imagemin@7.1.0  THIS VERSION WORKS !!! */
+//const imagemin = require('gulp-imagemin');
+//const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 /* ==================== EXAMPLE FOR TASKS
 function myTask(done)
@@ -25,6 +34,37 @@ function taskCSS(done){
     done();
 }
 
+/* npm install --save-dev gulp-imagemin@7.1.0  THIS VERSION WORKS !!! */
+function taskImages(done) {
+    const op = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(op) ) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function taskWebp( done ) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(options) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function taskAvif( done ) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}') 
+        .pipe( avif(options) )
+        .pipe( dest('./build/img') )
+    done();
+}
+
 function taskWatch(done) {
 
     /* watch ('file that listens', the calling function) */
@@ -32,5 +72,10 @@ function taskWatch(done) {
     console.log("Task for watch...");
     done();
 }
+
 exports.taskCSS = taskCSS; 
+//exports.taskImages = taskImages;
+//exports.taskWebp = taskWebp;
+exports.taskAvif = taskAvif;
 exports.taskWatch = taskWatch;
+//exports.taskWatch = parallel ( taskCSS,taskImages,taskWebp,taskAvif,taskWatch );
